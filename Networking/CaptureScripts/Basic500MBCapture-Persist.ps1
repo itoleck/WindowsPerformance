e6377326130a6e3cb9cfd613b1ad5dcd12a8c911
Chad Schultz <itoleck@gmail.com>
2022-02-16 16:57:38 -0500

    Networking updates

---
+++ Networking/CaptureScripts/Basic500MBCapture-Persist.ps1
@@ -0,0 +1,21 @@
+#requires –RunAsAdministrator
+
+Write-Host "Start 500MB circular capture. Press a key to save capture. Capture will automatically start again after reboot if not saved. Run this script again and press any key to save after reboot."
+Netsh trace start capture=yes persistent=yes
+pause
+Write-Host "Stopping and merging capture. This may take a few moments."
+Netsh trace stop
+Set-Location -Path "$env:USERPROFILE\AppData\Local\Temp\NetTraces"
+Get-ChildItem
+
+If (!(Test-Path "$env:USERPROFILE\Downloads\etl2pcapng.zip")) {
+    Write-Host "Downloading etl2pcapng from GitHub"
+    Invoke-WebRequest -Uri "https://github.com/microsoft/etl2pcapng/releases/download/v1.7.0/etl2pcapng.zip" -OutFile "$env:USERPROFILE\Downloads\etl2pcapng.zip"
+}
+If (!(Test-Path "$env:USERPROFILE\Downloads\etl2pcapng\x64\etl2pcapng.exe")) {
+    Write-Host "Expanding etl2pcapng to folder in Downloads folder"
+    Expand-Archive -Path "$env:USERPROFILE\Downloads\etl2pcapng.zip" -DestinationPath "$env:USERPROFILE\Downloads\etl2pcapng" -Force
+}
+
+Write-Host "Starting automatic conversion of network trace from .etl to .pcapng for WireShark analysis."
+Start-Process -FilePath "$env:USERPROFILE\Downloads\etl2pcapng\x64\etl2pcapng.exe" -ArgumentList "$env:USERPROFILE\AppData\Local\Temp\NetTraces\NetTrace.etl $env:USERPROFILE\AppData\Local\Temp\NetTraces\NetTrace.pcapng"
---
+++ Networking/CaptureScripts/Virtualization500MBCapture.ps1
@@ -0,0 +1,22 @@
+#requires –RunAsAdministrator
+
+Write-Host "Start 500MB circular capture. Press a key to save capture. Capture will not automatically start after reboot."
+Netsh trace start capture=yes scenario=NDIS,LAN,NetConnection,Virtualization,Layer2
+pause
+Write-Host "Stopping and merging capture. This may take a few moments."
+Netsh trace stop
+Set-Location -Path "$env:USERPROFILE\AppData\Local\Temp\NetTraces"
+Get-ChildItem
+
+If (!(Test-Path "$env:USERPROFILE\Downloads\etl2pcapng.zip")) {
+    Write-Host "Downloading etl2pcapng from GitHub"
+    Invoke-WebRequest -Uri "https://github.com/microsoft/etl2pcapng/releases/download/v1.7.0/etl2pcapng.zip" -OutFile "$env:USERPROFILE\Downloads\etl2pcapng.zip"
+}
+If (!(Test-Path "$env:USERPROFILE\Downloads\etl2pcapng\x64\etl2pcapng.exe")) {
+    Write-Host "Expanding etl2pcapng to folder in Downloads folder"
+    Expand-Archive -Path "$env:USERPROFILE\Downloads\etl2pcapng.zip" -DestinationPath "$env:USERPROFILE\Downloads\etl2pcapng" -Force
+}
+
+Write-Host "Starting automatic conversion of network trace from .etl to .pcapng for WireShark analysis."
+Write-Host ".etl file can also be opened in Event Viewer, WPA, Network Monitor or other tools for ETW event analysis."
+Start-Process -FilePath "$env:USERPROFILE\Downloads\etl2pcapng\x64\etl2pcapng.exe" -ArgumentList "$env:USERPROFILE\AppData\Local\Temp\NetTraces\NetTrace.etl $env:USERPROFILE\AppData\Local\Temp\NetTraces\NetTrace.pcapng"
