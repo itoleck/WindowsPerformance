f5405d86cb68cf11c492da8987316715e1eca6c7
Chad Schultz <itoleck@gmail.com>
2023-08-22 13:45:49 -0500

    Update Start-Circular-Light-CPU-Drivers-1GB.ps1

--- ETW/Tools/WPT/WPR/CaptureScripts/Start-Circular-Light-CPU-Drivers-1GB.ps1
+++ ETW/Tools/WPT/WPR/CaptureScripts/Start-Circular-Light-CPU-Drivers-1GB.ps1
@@ -5,12 +5,8 @@
 
 Write-Host "Start driver capture."
 
-If (!(Test-Path $env:TEMP)) {
-    New-Item -Path $env:TEMP
-}
-
 #Set the CPU profiling slower, 1s, still good enough for general CPU usage. Max is 10,000,000 (10s).
 wpr.exe -setprofint 10000
-wpr.exe -boottrace -start ..\Profiles\General-CPU-Drivers-Light-Circular-1GB.wprp -filemode -recordtempto $env:TEMP
+wpr.exe -start ..\Profiles\General-CPU-Drivers-Light-Circular-1GB.wprp
 
 Write-Host ("To stop tracing run :wpr.exe -stop <filename.etl>") -ForegroundColor Green
