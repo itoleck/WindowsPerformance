93335946d744f6f10eb6d6a3a3ae8a71a3101791
Chad Schultz <itoleck@gmail.com>
2023-08-22 13:42:20 -0500

    fix

--- ETW/Tools/WPT/WPR/CaptureScripts/Start-Circular-Light-CPU-Drivers-1GB.ps1
+++ ETW/Tools/WPT/WPR/CaptureScripts/Start-Circular-Light-CPU-Drivers-1GB.ps1
@@ -3,9 +3,7 @@
 #2022 Chad Schultz
 #https://github.com/itoleck/WindowsPerformance
 
-Write-Host "Start Ultra Low Impact Boot capture."
-Write-Host "After rebooting the machine run the save script (Save-BootTrace.ps1) to stop the capture and save the file."
-Write-Host "This is done so that the trace can be saved without the 2 minute auto-rundown time."
+Write-Host "Start driver capture."
 
 If (!(Test-Path $env:TEMP)) {
     New-Item -Path $env:TEMP
@@ -13,6 +11,6 @@ If (!(Test-Path $env:TEMP)) {
 
 #Set the CPU profiling slower, 1s, still good enough for general CPU usage. Max is 10,000,000 (10s).
 wpr.exe -setprofint 10000
-wpr.exe -boottrace -addboot ..\Profiles\General-CPU-Drivers-Light-Circular-1GB.wprp -filemode -recordtempto $env:TEMP
+wpr.exe -boottrace -start ..\Profiles\General-CPU-Drivers-Light-Circular-1GB.wprp -filemode -recordtempto $env:TEMP
 
-Write-Host "Please reboot the machine, login and run the Save-BootTrace.ps1 script as an administrator."
+Write-Host ("To stop tracing run :wpr.exe -stop <filename.etl>") -ForegroundColor Green
