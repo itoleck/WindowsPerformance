109be437ffd5769470bf135fc4c6a5226e1b0205
Chad Schultz <25645350+itoleck@users.noreply.github.com>
2023-11-09 10:10:00 -0600

    Capture script updates

--- ETW/Tools/WPT/WPR/CaptureScripts/Start-Circular-Verbose-CPU-ThreadDirector-Drivers-4GB.ps1
+++ ETW/Tools/WPT/WPR/CaptureScripts/Start-Circular-Verbose-CPU-ThreadDirector-Drivers-4GB.ps1
@@ -3,10 +3,10 @@
 #2022 Chad Schultz
 #https://github.com/itoleck/WindowsPerformance
 
-Write-Host "Start Windows Update Troubleshooting capture."
+Write-Host "Start verbose driver capture."
 
 #Set the CPU profiling slower, 1ms, still good enough for general CPU usage. Max is 10,000,000 (10s). Default 10,000 (1ms)
-wpr.exe -setprofint 1000000
-wpr.exe -start ..\Profiles\General-WindowsUpdateTroubleshooting.wprp
+wpr.exe -setprofint 10000
+wpr.exe -start ..\Profiles\General-CPU-ThreadDirector-Drivers-Verbose-Circular-4GB.wprp
 
 Write-Host ("To stop tracing run :wpr.exe -stop <filename.etl>") -ForegroundColor Green
